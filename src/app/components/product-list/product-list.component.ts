18c5cbbaf22f15fe78f1c1ba2b59a755ef29c36f
NightOutCoderAtGit <notalkonlycode@gmail.com>
2024-06-26 22:41:00 +0530

    pagination follow up

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -10,9 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductListComponent implements OnInit {
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   serachMode: boolean = false;
 
   products: Product[] = [];
+
+  // ne properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 5; // try with 5 & 50
+  theTotalElements: number = 0;
+
+
   constructor(private proudctService: ProudctService, private route: ActivatedRoute){
   }
 
@@ -44,9 +52,36 @@ export class ProductListComponent implements OnInit {
       // no category available. setting default to 1
       this.currentCategoryId = 1;
     }
-    this.proudctService.getProudctList(this.currentCategoryId).subscribe(
+
+    // check if we have a different category than previous
+    // Note - Angular will reuse a component if it is currently being viewed
+    //
+
+    // If we have a different category id than previous
+    // the set thePageNumber back to 1
+
+    if(this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+    /*Old Function*/
+    // this.proudctService.getProudctList(this.currentCategoryId).subscribe(
+    //   data => {
+    //     this.products = data;
+    //   }
+    // )
+
+
+    // here we setting -1 to pageNumber becuase in Spring data rest: pages are 0 based but in angular zero based
+    this.proudctService.getProductistPaginate(this.thePageNumber-1, this.thePageSize,this.currentCategoryId).subscribe(
       data => {
-        this.products = data;
+        this.products = data._embedded.products;
+        this.thePageNumber = data.page.number + 1; // pages are 0 based in spring data rest 
+        this.thePageSize = data.page.size;
+        this.theTotalElements = data.page.totalElements;
       }
     )
   }
@@ -58,5 +93,11 @@ export class ProductListComponent implements OnInit {
       this.products = data;
     }  
     );
+  }
+
+  updatePageSize(pageSize: string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProudcts();
   }
 }
