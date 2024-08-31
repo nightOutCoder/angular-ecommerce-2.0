d5172bdaf50c3a447899c3cc0e2673e54b68761d
NightOutCoderAtGit <notalkonlycode@gmail.com>
2024-09-01 00:39:45 +0530

    fix http at frontend level

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId:'0oajc3ihvqqBQdQPz5d7',
         issuer: 'https://dev-26155442.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
